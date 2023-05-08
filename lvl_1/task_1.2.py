9ab254ba35e12cbee5fec382b625d2c6d2798df9
LidiaKopylova <131188014+LidiaKopylova@users.noreply.github.com>
2023-05-08 17:10:22 +0300

    task 1.2
    task 1.2

---
+++ lvl_1/task_1.2.py
@@ -0,0 +1,40 @@
+# Пункт A. 
+# Приведем плейлист песен в виде списка списков
+# Список my_favorite_songs содержит список названий и длительности каждого трека
+# Выведите общее время звучания трех случайных песен в формате
+# Три песни звучат ХХХ минут
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+songs = my_favorite_songs[4][1] + my_favorite_songs[2][1] + my_favorite_songs[7][1]
+songs = round(songs, 2)
+print('Три песни звучат', songs)
+
+# Пункт B. 
+# Есть словарь песен 
+# Распечатайте общее время звучания трех случайных песен
+# Вывод: Три песни звучат ХХХ минут.
+my_favorite_songs_dict = {
+    'World in My Eyes': 4.76,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.30,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.6,
+    'Policy of Truth': 4.88,
+    'Blue Dress': 4.18,
+    'Clean': 5.68,
+}
+
+three_songs = my_favorite_songs_dict['Enjoy the Silence'] + my_favorite_songs_dict['Personal Jesus'] + \
+                      my_favorite_songs_dict['World in My Eyes']
+three_songs = round(three_songs, 2)
+print('Три песни звучат ', three_songs, " минут")
