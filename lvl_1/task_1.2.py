b13fe87d1882c710a19341399ea2941820027dfd
LidiaKopylova <131188014+LidiaKopylova@users.noreply.github.com>
2023-05-10 17:37:19 +0300

    Update task_1.2.py
    fixed 10.05

--- lvl_1/task_1.2.py
+++ lvl_1/task_1.2.py
@@ -3,8 +3,10 @@
 # Список my_favorite_songs содержит список названий и длительности каждого трека
 # Выведите общее время звучания трех случайных песен в формате
 # Три песни звучат ХХХ минут
+import random
+
 my_favorite_songs = [
-    ['Waste a Moment', 3.03],
+    ['Waste a Moment', 3.05],
     ['New Salvation', 4.02],
     ['Staying\' Alive', 3.40],
     ['Out of Touch', 3.03],
@@ -14,27 +16,38 @@ my_favorite_songs = [
     ['Nowhere to Run', 2.58],
     ['In This World', 4.02],
 ]
-songs = my_favorite_songs[4][1] + my_favorite_songs[2][1] + my_favorite_songs[7][1]
-songs = round(songs, 2)
-print('Три песни звучат ', songs, ' минут')
+random_index1 = random.randint(0, len(my_favorite_songs) - 1)
+print(my_favorite_songs[random_index1][0])
+random_index2 = random.randint(0, len(my_favorite_songs) - 1)
+print(my_favorite_songs[random_index2][0])
+random_index3 = random.randint(0, len(my_favorite_songs) - 1)
+print(my_favorite_songs[random_index3][0])
+print(
+    'Три песни звучат',
+    round(my_favorite_songs[random_index1][1] +
+          my_favorite_songs[random_index2][1] +
+          my_favorite_songs[random_index3][1]), 'минут')
 
 # Пункт B. 
 # Есть словарь песен 
 # Распечатайте общее время звучания трех случайных песен
 # Вывод: Три песни звучат ХХХ минут.
-my_favorite_songs_dict = {
-    'World in My Eyes': 4.76,
-    'Sweetest Perfection': 4.43,
-    'Personal Jesus': 4.56,
-    'Halo': 4.30,
-    'Waiting for the Night': 6.07,
-    'Enjoy the Silence': 4.6,
-    'Policy of Truth': 4.88,
-    'Blue Dress': 4.18,
-    'Clean': 5.68,
-}
 
-three_songs = my_favorite_songs_dict['Enjoy the Silence'] + my_favorite_songs_dict['Personal Jesus'] + \
-                      my_favorite_songs_dict['World in My Eyes']
-three_songs = round(three_songs, 2)
-print('Три песни звучат ', three_songs, ' минут')
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.05,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+song_1 = random.choice(list(my_favorite_songs_dict))
+time_1 = my_favorite_songs_dict[song_1]
+song_2 = random.choice(list(my_favorite_songs_dict))
+time_2 = my_favorite_songs_dict[song_2]
+song_3 = random.choice(list(my_favorite_songs_dict))
+time_3 = my_favorite_songs_dict[song_3]
+print('Три песни звучат', round(time_1 + time_2 + time_3), 'минут')
