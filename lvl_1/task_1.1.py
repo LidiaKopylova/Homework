3170ea8083a19c4674e6821588442df837f53146
LidiaKopylova <131188014+LidiaKopylova@users.noreply.github.com>
2023-05-10 17:41:18 +0300

    Update task_1.1.py
    fixed 10.05

--- lvl_1/task_1.1.py
+++ lvl_1/task_1.1.py
@@ -9,7 +9,7 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 string_length = len(my_favorite_songs)
 
-print(my_favorite_songs[0:13])
+print(my_favorite_songs[0:14])
 print(my_favorite_songs[-13:])
 print(my_favorite_songs[16:30])
 print(my_favorite_songs[-26:-15])
